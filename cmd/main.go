2e08ddba0d36f75e923acc8326feb73a0f3370ed
KeitaShimura <keitashimura2023@gmail.com>
2025-06-12 01:12:18 +0900

    feat: setup main.go

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Application has started")
+
+	// Some processing
+	log.Println("Processing...")
+
+	log.Println("Application has finished")
+}

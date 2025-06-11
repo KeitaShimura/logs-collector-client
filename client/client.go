590bac5505405228a3127d63b2f3adbd3e0ac8e1
KeitaShimura <keitashimura2023@gmail.com>
2025-06-12 01:38:01 +0900

    feat: add Client interface for logs

---
+++ client/client.go
@@ -0,0 +1,13 @@
+package client
+
+import (
+	"context"
+
+	"github.com/KeitaShimura/logs-collector-client/model"
+)
+
+// Client はログの送信および取得を行うためのインターフェース
+type Client interface {
+	SendLog(ctx context.Context, log *model.Log) error
+	GetLogs(ctx context.Context, service string, level string, limit int, offset int) ([]model.Log, error)
+}
